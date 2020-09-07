e565791949797463c996f55d10ed0f6ef1da49e5
Francois <francois.desazars@gmail.com>
2020-09-07 16:05:32 +0200

    implemented categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   has_one_attached :photo
   has_rich_text :rich_body
+  acts_as_taggable_on :categories
 
   validates :title, :photo, :rich_body, presence: true
 end
