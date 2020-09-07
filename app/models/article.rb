693289865abf247d61cb50ded96a7d7bd5c9e188
Francois <francois.desazars@gmail.com>
2020-09-07 15:22:26 +0200

    cloudinary setup

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
+  has_one_attached :photo
+  has_many_attached :article_photos
   has_rich_text :rich_body
+
+  validates :title, :photo, :rich_body, presence: true
 end
