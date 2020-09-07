bfac1351c78041c0bcf10d8dacab3ebf0074d6b4
Francois <francois.desazars@gmail.com>
2020-09-07 15:52:52 +0200

    changed root path and added article index]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'articles#index'
-  resources :articles, except: :index
+  resources :articles
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
