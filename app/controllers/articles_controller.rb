998108e7898739d97db3318923f6b013729359e8
Francois <francois.desazars@gmail.com>
2020-09-07 15:29:57 +0200

    cloudinary implementation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -69,6 +69,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def article_params
-      params.require(:article).permit(:title, :rich_body)
+      params.require(:article).permit(:title, :rich_body, :photo)
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,5 @@
 class Article < ApplicationRecord
   has_one_attached :photo
-  has_many_attached :article_photos
   has_rich_text :rich_body
 
   validates :title, :photo, :rich_body, presence: true
